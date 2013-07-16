a780c3927c31f6c328e2f139d16068bc1bca08f1
David Möbius <d.moebius@tarent.de>
2013-07-16 14:54:30 +0200

    New Integration Tests added for testuser values. files added to add a test user

--- src/test/java/org/osiam/client/UserServiceIT.java
+++ src/test/java/org/osiam/client/UserServiceIT.java
@@ -11,14 +11,18 @@ import com.sun.jersey.api.client.UniformInterfaceException;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class UserServiceIT {
 
-    private String accessToken = "5dffcca8-59be-46d2-90c7-fbc6fb8451ac";
-    private String validUserUUID = "4a51b8fa-e4c5-4164-8864-b52a1bb5ca17";
+    private String accessToken = "83e1fe67-3259-486b-9b0f-774e51537c6b";
+    private String uuidStandardUser = "94bbe688-4b1e-4e4e-80e7-e5ba5c4d6db4";
     private String endpointAddress = "http://localhost:8080/osiam-server";
     private URI serviceEndpoint;
     private String redirectAddress = "http://localhost:5000/oauth2";
@@ -40,11 +44,47 @@ public class UserServiceIT {
 
     @Test
     public void getValidUser() {
-        User user = service.getUserByUUID(UUID.fromString(validUserUUID), accessToken);
+        User user = service.getUserByUUID(UUID.fromString(uuidStandardUser), accessToken);
+        assertEquals(uuidStandardUser, user.getId());
+    }
+    
+    @Test
+    public void ensureValuesStandardUser() throws ParseException {
+        User actualUser = service.getUserByUUID(UUID.fromString(uuidStandardUser), accessToken);
 
-        assertEquals(validUserUUID, user.getId());
-        assertEquals("tobias", user.getExternalId());
-        assertEquals(null, user.getNickName());
+        assertEquals("User", actualUser.getMeta().getResourceType());
+        Date created = new SimpleDateFormat( "dd.MM.yyyy HH:mm:ss" ).parse("01.08.2011 18:29:49");
+        assertEquals(created, actualUser.getMeta().getCreated());
+        assertEquals(created, actualUser.getMeta().getLastModified());      
+        assertEquals(uuidStandardUser, actualUser.getId());
+        assertEquals(1, actualUser.getAddresses().size());
+        assertEquals("example street 42", actualUser.getAddresses().get(0).getStreetAddress());
+        assertEquals("germany", actualUser.getAddresses().get(0).getCountry());
+        assertEquals("11111", actualUser.getAddresses().get(0).getPostalCode());
+        assertEquals("MaxExample", actualUser.getDisplayName());
+        assertEquals(2, actualUser.getEmails().size());
+        String email = actualUser.getEmails().get(0).getValue().toString();
+        boolean exists = email.equals("MaxExample@work.com") || email.equals("MaxExample@home.de"); 
+        assertTrue(exists);
+        email = actualUser.getEmails().get(1).getValue().toString();
+        exists = email.equals("MaxExample@work.com") || email.equals("MaxExample@home.de");
+        assertTrue(exists);
+        assertEquals("MExample", actualUser.getExternalId());
+        assertEquals("de", actualUser.getLocale());
+        assertEquals("Example", actualUser.getName().getFamilyName());
+        assertEquals("Max", actualUser.getName().getGivenName());
+        assertEquals("Jason", actualUser.getName().getMiddleName());
+        assertEquals("Max", actualUser.getNickName());
+        assertEquals(null, actualUser.getPassword());
+        assertEquals(1, actualUser.getPhoneNumbers().size());
+        assertEquals("666-999-6666", actualUser.getPhoneNumbers().get(0).getValue().toString());
+        assertEquals("de", actualUser.getPreferredLanguage());
+        assertEquals("http://test.de", actualUser.getProfileUrl());
+        assertEquals("UTC", actualUser.getTimezone());
+        assertEquals("Dr", actualUser.getTitle());
+        assertEquals("MaxExample", actualUser.getUserName());
+        assertEquals("User", actualUser.getUserType());
+        assertEquals(null, actualUser.isActive());
     }
     
     @Test(expected = UniformInterfaceException.class)
