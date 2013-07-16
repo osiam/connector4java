1c97adf2e6a27a82c75c1c0c8145e5ae722515c5
David Möbius <d.moebius@tarent.de>
2013-07-16 15:00:42 +0200

    JavaDoc added

--- src/main/java/org/osiam/client/exception/ConnectionInitializationException.java
+++ src/main/java/org/osiam/client/exception/ConnectionInitializationException.java
@@ -1,7 +1,14 @@
 package org.osiam.client.exception;
 
+/**
+ * will be thrown if the connection to the given OSIAM server could not be opened
+ *
+ */
 public class ConnectionInitializationException extends RuntimeException {
-    ConnectionInitializationException(String message) {
+
+	private static final long serialVersionUID = 1L;
+
+	ConnectionInitializationException(String message) {
         super(message);
     }
 
