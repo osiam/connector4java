5814e493e08faf4bda9fa394655af2f626a7dfff
Florian Wallner <wallner@balumba.org>
2013-07-19 12:50:15 +0200

    Javadoc Cleanups

--- src/main/java/org/osiam/client/exception/UnauthorizedException.java
+++ src/main/java/org/osiam/client/exception/UnauthorizedException.java
@@ -1,8 +1,8 @@
 package org.osiam.client.exception;
 
 /**
- * will be thrown if the actual session is not authorized to access the OSIAM service.
- * For example if the access token is not valid anymore
+ * This exception is thrown if the current session is not authorized to access the OSIAM service.
+ * For example if the access token expired.
  *
  */
 public class UnauthorizedException extends RuntimeException {
