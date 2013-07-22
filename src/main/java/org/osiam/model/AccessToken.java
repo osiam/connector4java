c63248f7dba08e8124f1eba884ab53e1081c4c5d
David Möbius <d.moebius@tarent.de>
2013-07-22 13:19:30 +0200

    AccessToken refactored

--- src/main/java/org/osiam/model/AccessToken.java
+++ src/main/java/org/osiam/model/AccessToken.java
@@ -1,54 +1,169 @@
 package org.osiam.model;
+/*
+ * for licensing see in the license.txt
+ */
+import java.util.Date;
 
 /**
- * Created with IntelliJ IDEA.
- * User: dmoeb
- * Date: 10.06.13
- * Time: 09:04
- * To change this template use File | Settings | File Templates.
+* This model class holds the information about an access token.
+* The access token is used to get access to a OSIAM server and to get and 
+* manipulate users and groups.
+* An instance of this class get be created by the AuthService.
  */
 public class AccessToken {
-    String access_token;
-    String token_type;
-    long expires_in;
-    String scope;
+	private String authId;
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType;
+    private long expiresIn;
+    private String scope;
+    private Date createdOn;
+    private Date expiresAt;
 
-    public String getAccess_token() {
-        return access_token;
+    /**
+     * Retrieve the Authentication ID used to retrieve the AccessToken
+     * @return The AuthId string
+     */
+	public String getAuthId() {
+		return authId;
+	}
+
+	/**
+	 * Set the Authentication ID to relate between the this ID and the AccessToken
+	 * retrieved with this one
+	 * @param authId The AuthId as string
+	 */
+	public void setAuthId(String authId) {
+		this.authId = authId;
+	}
+	
+	/**
+	 * Retrieve the string value of the AccessToken
+	 * @return The access token string
+	 */
+    public String getAccessToken() {
+        return accessToken;
     }
 
-    public void setAccess_token(String access_token) {
-        this.access_token = access_token;
+	/**
+	 * Set the string value of the AccessToken
+	 * @param accessToken The access token string
+	 */
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
     }
 
-    public String getToken_type() {
-        return token_type;
+    /**
+     * TODO is it possible to do a refreshing with OSIAM?
+     * @return
+     */
+	public String getRefreshToken() {
+		return refreshToken;
+	}
+
+	/**
+	 * TODO is it possible to do a refreshing with OSIAM?
+	 * @param refreshToken
+	 */
+	public void setRefreshToken(String refreshToken) {
+		this.refreshToken = refreshToken;
+	}
+    
+    /**
+     * TODO not sure which types are possible and why we need them
+     * @return
+     */
+    public String getTokenType() {
+        return tokenType;
     }
 
-    public void setToken_type(String token_type) {
-        this.token_type = token_type;
+    /**
+     * TODO not sure which types are possible and why we need them
+     * @param tokenType
+     */
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
     }
 
-    public long getExpires_in() {
-        return expires_in;
+    /**
+     *TODO do we need this if we have the expiresAt Date
+     * @return
+     */
+    public long getExpiresIn() {
+        return expiresIn;
+    }
+    /**
+     * TODO do we need this if we have the expiresAt Date
+     * @param expiresIn
+     */
+    public void setExpiresIn(long expiresIn) {
+        this.expiresIn = expiresIn;
     }
 
-    public void setExpires_in(long expires_in) {
-        this.expires_in = expires_in;
-    }
-
+    /**
+     * Retrieve the possible Scopes of this AccessToken
+     * @return The scopes as string
+     */
     public String getScope() {
         return scope;
     }
 
+    /**
+     * Set the wanted scopes of the AccessToken
+     * @param scope the scopes as string
+     */
     public void setScope(String scope) {
         this.scope = scope;
     }
 
+    /**
+     * Retrieve the Date when the AccessToken was created
+     * @return The creation Date
+     */
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+
+	/**
+	 * Set the Date when the AccessToken was created
+	 * @param createdOn The creation Date
+	 */
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	/**
+	 * Retrieve the Date when the AccessToken will be expired 
+	 * @return The expiring Date of the AccessToken
+	 */
+	public Date getExpiresAt() {
+		return expiresAt;
+	}
+
+	/**
+	 * Set the Expire Date when the AccessToken after 
+	 * the AccessToken will not be valid anymore
+	 * @param expiresAt The expiring Date
+	 */
+	public void setExpiresAt(Date expiresAt) {
+		this.expiresAt = expiresAt;
+	}
+	
     @Override
+    /**
+     * retrieves all main Information string based
+     */
     public String toString() {
-        return "AccessToken [access_token=" + access_token + ", token_type="
-                + token_type + ", expires_in=" + expires_in + ", scope="
-                + scope + "]";
+    	StringBuilder returnToken = new StringBuilder();
+    	
+    	returnToken.append("AccessToken [").
+    				append("access token = ").append(accessToken).
+    				append(", token type = ").append(tokenType).
+    				append(", scope = ").append(scope).
+    				append(", created on = ").append(createdOn).
+    				append(", expired at = ").append(expiresAt).
+    				append("]");
+    			
+        return returnToken.toString();
     }
 }
