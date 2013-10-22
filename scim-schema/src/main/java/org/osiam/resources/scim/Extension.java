e7b65dbe1df3aba1758fbb27fa500e814ffc5588
Florian Wallner <wallner@balumba.org>
2013-10-22 18:28:59 +0200

    First idea of scim schema extension representation

---
+++ scim-schema/src/main/java/org/osiam/resources/scim/Extension.java
@@ -0,0 +1,52 @@
+package org.osiam.resources.scim;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+/**
+ * The extension class models a deserialized view of schema extensions as specified by the scim 2.0 specification.
+ */
+public class Extension {
+
+    private Map<String, String> fields = new HashMap<>();
+
+    /**
+     * Return the value for the field with a given name
+     * @param field The name of the field to retrieve the value of.
+     * @return The value for the field with the given name.
+     * @throws NoSuchElementException if this schema does not contain a field of the given name.
+     * @throws IllegalArgumentException if the given field is null or an empty string.
+     */
+    public String getField(String field){
+        if (field == null || field.isEmpty()){
+            throw new IllegalArgumentException("Invalid field name");
+        }
+        if (! fields.containsKey(field)){
+            throw new NoSuchElementException("Field " + field + " not valid in this extension");
+        }
+        return fields.get(field);
+    }
+
+    /**
+     *  Add or update the field with the given name to the given value.
+     * @param field The name of the field whose value to set
+     * @param value The new value of the field.
+     * @throws IllegalArgumentException if the given field is null or an empty string.
+     */
+    public void setField(String field, String value){
+        if (field == null || field.isEmpty()){
+            throw new IllegalArgumentException("Invalid field name");
+        }
+        fields.put(field, value);
+    }
+
+    /**
+     * Provide a unmodifiable view on the entries in this schema as a map.
+     * @return The Entries of this schema as an map.
+     */
+    public Map<String, String> getAllFields(){
+        return Collections.unmodifiableMap(fields);
+    }
+}
