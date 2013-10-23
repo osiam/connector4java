f63970e9b9a59863a6cd90a74df6ff03f449ee35
Thomas Krille <t.krille@tarent.de>
2013-10-23 11:01:22 +0200

    [cleanup] cleaning up tests

--- scim-schema/src/test/groovy/org/osiam/resources/scim/UserTest.groovy
+++ scim-schema/src/test/groovy/org/osiam/resources/scim/UserTest.groovy
@@ -35,14 +35,14 @@ class UserTest extends Specification {
         user != null
     }
 
-    def "user should contain core schemas as default"() {
+    def "should contain core schemas as default"() {
         when:
         def user = new User.Builder("username").build()
         then:
         user.schemas == Constants.CORE_SCHEMAS;
     }
 
-    def "user should be able to create an user without name for PATCH"() {
+    def "should be possible to create an user without name for PATCH"() {
         when:
         def user = new User.Builder().build()
         then:
@@ -51,7 +51,7 @@ class UserTest extends Specification {
     }
 
 
-    def "user should be able to contain schemas"() {
+    def "should be able to contain schemas"() {
         def schemas = ["urn:wtf", "urn:hajo"] as Set
         when:
         User user = new User.Builder("username").setSchemas(schemas).build()
@@ -60,7 +60,7 @@ class UserTest extends Specification {
 
     }
 
-    def "user should clone schemas"() {
+    def "generateForOutput should copy schemas"() {
         def schemas = ["urn:wtf", "urn:hajo"] as Set
         User oldUser = new User.Builder("username").setSchemas(schemas).build()
         when:
@@ -70,7 +70,7 @@ class UserTest extends Specification {
 
     }
 
-    def "userName is a required field so it should throw an exception when setting it null"() {
+    def "using null for userName raises exception"() {
         when:
         new User.Builder(null)
         then:
@@ -127,57 +127,20 @@ class UserTest extends Specification {
         user.meta == builder.meta
     }
 
-    def "should ignore password on output"() {
+    def "generateForOutput should remove the password"() {
         given:
-        User user = new User.Builder("test").setActive(true)
-                .setDisplayName("display")
-                .setLocale("locale")
-                .setName(new Name.Builder().build())
-                .setNickName("nickname")
+        User user = new User.Builder("test")
                 .setPassword("password")
-                .setPreferredLanguage("prefereedLanguage")
-                .setProfileUrl("profileUrl")
-                .setTimezone("time")
-                .setTitle("title")
-                .setUserType("userType")
-                .setExternalId("externalid").setId("id").setMeta(new Meta.Builder().build())
                 .build()
         when:
         User clonedUser = User.Builder.generateForOutput(user)
         then:
         clonedUser.password == null
-
-        clonedUser.timezone == user.timezone
-        clonedUser.title == user.title
-        clonedUser.userType == user.userType
-        clonedUser.userName == user.userName
-        clonedUser.id == user.id
-        clonedUser.externalId == user.externalId
-        clonedUser.meta == user.meta
-        clonedUser.active == user.active
-        clonedUser.displayName == user.displayName
-        clonedUser.locale == user.locale
-        clonedUser.name == user.name
-        clonedUser.nickName == user.nickName
-        clonedUser.preferredLanguage == user.preferredLanguage
-        clonedUser.profileUrl == user.profileUrl
     }
 
     def "should set empty lists for pretty output"() {
         given:
-        User user = new User.Builder("test").setActive(true)
-                .setDisplayName("display")
-                .setLocale("locale")
-                .setName(new Name.Builder().build())
-                .setNickName("nickname")
-                .setPassword("password")
-                .setPreferredLanguage("prefereedLanguage")
-                .setProfileUrl("profileUrl")
-                .setTimezone("time")
-                .setTitle("title")
-                .setUserType("userType")
-                .setExternalId("externalid").setId("id").setMeta(new Meta.Builder().build())
-                .build()
+        User user = new User.Builder("test").build()
         when:
         User clonedUser = User.Builder.generateForOutput(user)
         then:
@@ -192,34 +155,22 @@ class UserTest extends Specification {
         clonedUser.x509Certificates.empty
     }
 
-    def "should copy lists from origin user"() {
+    def "generateForOutput should copy lists from original user"() {
         given:
         def address = new Address.Builder().build()
         def generalAttribute = new MultiValuedAttribute.Builder().build()
 
-        User user = new User.Builder("test").setActive(true)
-                .setDisplayName("display")
-                .setLocale("locale")
-                .setName(new Name.Builder().build())
-                .setNickName("nickname")
-                .setPassword("password")
-                .setPreferredLanguage("prefereedLanguage")
-                .setProfileUrl("profileUrl")
-                .setTimezone("time")
-                .setTitle("title")
-                .setUserType("userType")
-                .setExternalId("externalid").setId("id").setMeta(new Meta.Builder().build())
-                .build()
-
-        user.addresses.add(address)
-        user.emails.add(generalAttribute)
-        user.entitlements.add(generalAttribute)
-        user.groups.add(generalAttribute)
-        user.ims.add(generalAttribute)
-        user.phoneNumbers.add(generalAttribute)
-        user.photos.add(generalAttribute)
-        user.roles.add(generalAttribute)
-        user.x509Certificates.add(generalAttribute)
+        User user = new User.Builder("test")
+            .setAddresses([address])
+            .setEmails([generalAttribute])
+            .setEntitlements([generalAttribute])
+            .setGroups([generalAttribute])
+            .setIms([generalAttribute])
+            .setPhoneNumbers([generalAttribute])
+            .setPhotos([generalAttribute])
+            .setRoles([generalAttribute])
+            .setX509Certificates([generalAttribute])
+            .build()
 
         when:
         User clonedUser = User.Builder.generateForOutput(user)
@@ -244,34 +195,26 @@ class UserTest extends Specification {
         def generalAttribute = new MultiValuedAttribute.Builder().build()
 
         when:
-        user.addresses.add(address)
-        user.emails.add(generalAttribute)
-        user.entitlements.add(generalAttribute)
-        user.groups.add(generalAttribute)
-        user.ims.add(generalAttribute)
-        user.phoneNumbers.add(generalAttribute)
-        user.photos.add(generalAttribute)
-        user.roles.add(generalAttribute)
-        user.x509Certificates.add(generalAttribute)
+        user.getAddresses().add(address)
+        user.getEmails().add(generalAttribute)
+        user.getEntitlements().add(generalAttribute)
+        user.getGroups().add(generalAttribute)
+        user.getIms().add(generalAttribute)
+        user.getPhoneNumbers().add(generalAttribute)
+        user.getPhotos().add(generalAttribute)
+        user.getRoles().add(generalAttribute)
+        user.getX509Certificates().add(generalAttribute)
 
-        and:
-        user.addresses.get(0) == address
-        and:
-        user.emails.get(0) == generalAttribute
-        and:
-        user.entitlements.get(0) == generalAttribute
-        and:
-        user.groups.get(0) == generalAttribute
-        and:
-        user.ims.get(0) == generalAttribute
-        and:
-        user.phoneNumbers.get(0) == generalAttribute
-        and:
-        user.photos.get(0) == generalAttribute
-        and:
-        user.roles.get(0) == generalAttribute
         then:
+        user.addresses.get(0) == address
+        user.emails.get(0) == generalAttribute
+        user.entitlements.get(0) == generalAttribute
+        user.groups.get(0) == generalAttribute
+        user.ims.get(0) == generalAttribute
+        user.phoneNumbers.get(0) == generalAttribute
+        user.photos.get(0) == generalAttribute
+        user.roles.get(0) == generalAttribute
         user.x509Certificates.get(0) == generalAttribute
     }
-
+    
 }
