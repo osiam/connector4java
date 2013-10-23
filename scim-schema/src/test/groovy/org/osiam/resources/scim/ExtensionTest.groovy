333f0a53739f10d842533a2fafe31051e6557208
Thomas Krille <t.krille@tarent.de>
2013-10-23 11:23:39 +0200

    [task] finalized implementation #OSNG-191

--- scim-schema/src/test/groovy/org/osiam/resources/scim/ExtensionTest.groovy
+++ scim-schema/src/test/groovy/org/osiam/resources/scim/ExtensionTest.groovy
@@ -1,5 +1,8 @@
 package org.osiam.resources.scim
 
+import org.codehaus.groovy.classgen.Verifier.DefaultArgsAction;
+import org.codehaus.groovy.transform.sc.StaticCompilationMetadataKeys;
+
 import spock.lang.Specification
 
 
@@ -7,10 +10,13 @@ class ExtensionTest extends Specification {
 
     static def FIELD = 'foo'
     static def VALUE = 'bar'
+    
+    static def FIELD_INJECTED = 'injected'
+    static def VALUE_INJECTED = 'iwasinjected'
 
     def 'Query for existing field returns value'(){
         given:
-            def extension = new Extension(fields:[(FIELD):VALUE])
+            def extension = new Extension([(FIELD):VALUE])
         expect:
             extension.getField(FIELD) == VALUE
     }
@@ -44,24 +50,6 @@ class ExtensionTest extends Specification {
             thrown(IllegalArgumentException)
     }
 
-    def 'Setting a valid element adds it to fields'(){
-        given:
-            def extension = new Extension()
-        when:
-            extension.setField(FIELD,VALUE)
-        then:
-            extension.fields[FIELD] == VALUE
-    }
-
-    def 'Setting a field to an empty string raises IllegalArgumentException'(){
-        given:
-            def extension = new Extension()
-        when:
-            extension.setField('', VALUE)
-        then:
-            thrown(IllegalArgumentException)
-    }
-
     def 'Setting a field to null raises IllegalArgumentException'(){
         given:
             def extension = new Extension()
@@ -70,10 +58,19 @@ class ExtensionTest extends Specification {
         then:
             thrown(IllegalArgumentException)
     }
+    
+    def 'Adding a field raises exception' () {
+        given:
+            def extension = new Extension([(FIELD):VALUE, (VALUE):FIELD])
+        when:
+            extension.setField(FIELD_INJECTED, VALUE_INJECTED)
+        then:
+            thrown(IllegalArgumentException)
+    }
 
     def 'getAllFields returns a map of all the fields'(){
         given:
-            def extension = new Extension(fields: [(FIELD):VALUE, (VALUE):FIELD])
+            def extension = new Extension([(FIELD):VALUE, (VALUE):FIELD])
         when:
             def result = extension.allFields
         then:
@@ -84,11 +81,22 @@ class ExtensionTest extends Specification {
 
     def 'getAllFields returns a immutable map'(){
         given:
-            def extension = new Extension(fields: [(FIELD):VALUE])
+            def extension = new Extension([(FIELD):VALUE])
             def result = extension.getAllFields()
         when:
             result[FIELD] = FIELD
         then:
             thrown(UnsupportedOperationException)
+    }
+    
+    def 'the constructor creates a copy of its parameter' () {
+        given:
+            def map = [(FIELD):VALUE, (VALUE):FIELD]
+            def extension = new Extension(map)
+        when:
+            map[FIELD_INJECTED] = VALUE_INJECTED
+            
+        then:
+            !extension.fields.containsKey(FIELD_INJECTED)
     }
 }
