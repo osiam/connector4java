4096102c960999cdb5de3bf9df768b1f05a77d92
jtodea <jochen.todea@googlemail.com>
2013-10-24 16:47:17 +0200

    fixed Constants.CORE_SCHEMA change problems in the server and in SCIMSearchResults. was Set before and is String now

--- scim-schema/src/test/groovy/org/osiam/resources/scim/SCIMSearchResultTest.groovy
+++ scim-schema/src/test/groovy/org/osiam/resources/scim/SCIMSearchResultTest.groovy
@@ -14,6 +14,23 @@ class SCIMSearchResultTest extends Specification {
     def "parametrized constructor and corresponding getter should be present"(){
         given:
         def result = ["searchResult"] as List
+        def schema = "schema1"
+        def schemas = [schema] as Set
+
+        when:
+        def a = new SCIMSearchResult(result, 2342, 100, 1, schema)
+
+        then:
+        a.resources == result
+        a.totalResults == 2342
+        a.itemsPerPage == 100
+        a.startIndex == 1
+        a.schemas == schemas
+    }
+
+    def "parametrized constructor with schema collection should be present"(){
+        given:
+        def result = ["searchResult"] as List
         def schemas = ["schema1"] as Set
 
         when:
