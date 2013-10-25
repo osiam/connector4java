116e46c57d445afb43475493a26017eede6c532b
Thomas Krille <t.krille@tarent.de>
2013-10-25 12:30:42 +0200

    [enhancement] switching from file-based to template-based test data

--- scim-schema/src/test/groovy/org/osiam/resources/helper/UserDeserializerSpec.groovy
+++ scim-schema/src/test/groovy/org/osiam/resources/helper/UserDeserializerSpec.groovy
@@ -1,5 +1,8 @@
 package org.osiam.resources.helper
 
+import groovy.text.SimpleTemplateEngine
+import groovy.text.Template
+
 import org.codehaus.jackson.JsonProcessingException
 import org.codehaus.jackson.Version
 import org.codehaus.jackson.map.JsonMappingException
@@ -7,25 +10,27 @@ import org.codehaus.jackson.map.ObjectMapper
 import org.codehaus.jackson.map.module.SimpleModule
 import org.osiam.resources.scim.Extension
 import org.osiam.resources.scim.User
-import spock.lang.Ignore
+
+import spock.lang.Ignore;
 import spock.lang.Specification
 import spock.lang.Unroll
 
 
 class UserDeserializerSpec extends Specification {
 
-    private static final String BASIC_FILE_PATH = 'src/test/resources/basicUser.json'
-    private static final String EXTENDED_FILE_PATH = 'src/test/resources/extendedUser.json'
-    private static final String INVALID_FILE_PATH = 'src/test/resources/invalidExtendedUser.json'
-    private static final String WRONG_FIELD_FILE_PATH = 'src/test/resources/wrongFieldExtendedUser.json'
+    private static final SimpleTemplateEngine TEMPLATE_ENGINE = new SimpleTemplateEngine()
+    private static final String JSON_USER = '{"id":"a4bbe688-4b1e-4e4e-80e7-e5ba5c4d6db4","meta":{"created":"2011-08-01T20:29:49.000+02:00","lastModified":"2011-08-01T20:29:49.000+02:00","location":"https://example.com/v1/Users/2819c223...","resourceType":"User"},"schemas":[$schemasMore"urn:scim:schemas:core:1.0"],"externalId":"bjensen","userName":"bjensen","name":{"formatted":"Ms. Barbara J Jensen III","familyName":"Jensen","givenName":"Barbara"},"displayName":"BarbaraJ.","nickName":"Barbara","title":"Dr.","locale":"de","emails":[{"value":"bjensen@example.com","type":"work"}],"phoneNumbers":[{"value":"555-555-8377","type":"work"}],"addresses":[{"type":"work","streetAddress":"example street 42","locality":"Germany","region":"Germany","postalCode":"11111","country":"Germany"}]$dataMore}'
+    private static final String JSON_PARTITIAL_EXTENSION = ',"urn:scim:schemas:extension:enterprise:2.0:User":{"organization":"Universal Studios","department":"Tour Operations","employeeNumber":"701984"}'
+    private static final String JSON_PARTITIAL_WRONG_TYPE_EXTENSION = ',"urn:scim:schemas:extension:enterprise:2.0:User": [10]'
     private static final String ENTERPRISE_URN = 'urn:scim:schemas:extension:enterprise:2.0:User'
+    private static final String JSON_PARTITIAL_ENTERPRISE_URN = '"urn:scim:schemas:extension:enterprise:2.0:User",'
+    private static final Template TEMPLATE = TEMPLATE_ENGINE.createTemplate(JSON_USER)
 
     def 'Return an User Instance'(){
         when:
             User user = mapExtendedUser()
         then:
             user instanceof User
-
     }
 
     def 'A valid basic user is returned'(){
@@ -63,10 +68,10 @@ class UserDeserializerSpec extends Specification {
         then:
             extension.getField(fieldName) == fieldValue
         where:
-        fieldName        | fieldValue
-        'employeeNumber' | '701984'
-        'organization'   | 'Universal Studios'
-        'department'     | 'Tour Operations'
+            fieldName        | fieldValue
+            'employeeNumber' | '701984'
+            'organization'   | 'Universal Studios'
+            'department'     | 'Tour Operations'
     }
 
     def 'Extension schema registered but missing field raises exception'(){
@@ -74,7 +79,6 @@ class UserDeserializerSpec extends Specification {
             mapInvalidExtendedUser()
         then:
             thrown(JsonProcessingException)
-
     }
 
     def 'Extension of wrong JSON type raises exception'(){
@@ -93,15 +97,19 @@ class UserDeserializerSpec extends Specification {
     }
 
     def mapBasicUser(){
-       configuredObjectMapper().readValue(new File(BASIC_FILE_PATH), User)
+        def jsonString = TEMPLATE.make([schemasMore:'', dataMore:'']).toString()
+        configuredObjectMapper().readValue(jsonString, User)
     }
     def mapExtendedUser(){
-        configuredObjectMapper().readValue(new File(EXTENDED_FILE_PATH), User)
+        def jsonString = TEMPLATE.make([schemasMore:JSON_PARTITIAL_ENTERPRISE_URN, dataMore:JSON_PARTITIAL_EXTENSION]).toString()
+        configuredObjectMapper().readValue(jsonString, User)
     }
     def mapInvalidExtendedUser(){
-        configuredObjectMapper().readValue(new File(INVALID_FILE_PATH), User)
+        def jsonString = TEMPLATE.make([schemasMore:JSON_PARTITIAL_ENTERPRISE_URN, dataMore:'']).toString()
+        configuredObjectMapper().readValue(jsonString, User)
     }
     def mapWrongFieldExtendedUser(){
-        configuredObjectMapper().readValue(new File(WRONG_FIELD_FILE_PATH), User)
+        def jsonString = TEMPLATE.make([schemasMore:JSON_PARTITIAL_ENTERPRISE_URN, dataMore:JSON_PARTITIAL_WRONG_TYPE_EXTENSION]).toString()
+        configuredObjectMapper().readValue(jsonString, User)
     }
 }
