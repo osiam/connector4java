71b8a72b1975e69bb1f9f28145e15ce77fdae3a3
xqf23 <tkrille@gmail.com>
2013-11-22 15:13:44 +0100

    [enhancement] added schema for Service Provider Config

--- scim-schema/src/main/java/org/osiam/resources/scim/Constants.java
+++ scim-schema/src/main/java/org/osiam/resources/scim/Constants.java
@@ -28,5 +28,6 @@ public interface Constants {
 
 	 String USER_CORE_SCHEMA = "urn:scim:schemas:core:2.0:User";
 	 String GROUP_CORE_SCHEMA = "urn:scim:schemas:core:2.0:Group";
+	 String SERVICE_PROVIDER_CORE_SCHEMA = "urn:scim:schemas:core:2.0:ServiceProviderConfig";
     int MAX_RESULT = 100;
 }
