3dab08e57bfd7c660799d3360892b62a15318fd3
David Möbius <d.moebius@tarent.de>
2013-12-03 10:57:58 +0100

    adding new BasicUser Object

---
+++ src/main/java/org/osiam/client/minimalUser/BasicUser.java
@@ -0,0 +1,198 @@
+/*
+ * Copyright (C) 2013 tarent AG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.osiam.client.minimalUser;
+
+import java.util.Date;
+
+import org.osiam.resources.helper.JsonDateSerializer;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+/**
+ * <p>Java class for User complex type.
+ * <p/>
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ */
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_EMPTY)
+@JsonIgnoreProperties({"link", "gender", "timezone", "verified"})
+public class BasicUser {
+
+    private String id;
+    private String name = "";
+    @JsonProperty("first_name")
+    private String firstName = "";
+    @JsonProperty("last_name")
+    private String lastName = "";
+    private String userName = "";
+    private String email = "";
+    private String locale = "";
+    @JsonProperty("updated_time")
+    @JsonSerialize(using = JsonDateSerializer.class)
+    private Date updatedTime;
+
+    public BasicUser() {
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    /**
+     * Gets the formatted name property.
+     *
+     * @return possible object is
+     *         {@link String }
+     */
+    public String getName() {
+        return name;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    /**
+     *
+     * @return the primary email address of the user
+     */
+    public String getEmail(){
+        return email;
+    }
+    /**
+     * Gets the value of the userName property.
+     *
+     * @return possible object is
+     *         {@link String }
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    public Date getUpdatedTime(){
+        return updatedTime;
+    }
+
+    /**
+     * Gets the value of the locale property.
+     *
+     * @return possible object is
+     *         {@link String }
+     */
+    public String getLocale() {
+        return locale;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        result = prime * result + ((locale == null) ? 0 : locale.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((updatedTime == null) ? 0 : updatedTime.hashCode());
+        result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        BasicUser other = (BasicUser) obj;
+        if (email == null) {
+            if (other.email != null) {
+                return false;
+            }
+        } else if (!email.equals(other.email)) {
+            return false;
+        }
+        if (firstName == null) {
+            if (other.firstName != null) {
+                return false;
+            }
+        } else if (!firstName.equals(other.firstName)) {
+            return false;
+        }
+        if (id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } else if (!id.equals(other.id)) {
+            return false;
+        }
+        if (lastName == null) {
+            if (other.lastName != null) {
+                return false;
+            }
+        } else if (!lastName.equals(other.lastName)) {
+            return false;
+        }
+        if (locale == null) {
+            if (other.locale != null) {
+                return false;
+            }
+        } else if (!locale.equals(other.locale)) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        if (updatedTime == null) {
+            if (other.updatedTime != null) {
+                return false;
+            }
+        } else if (!updatedTime.equals(other.updatedTime)) {
+            return false;
+        }
+        if (userName == null) {
+            if (other.userName != null) {
+                return false;
+            }
+        } else if (!userName.equals(other.userName)) {
+            return false;
+        }
+        return true;
+    }
+
+}
