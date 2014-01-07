c3cae0c2282f4df16bc6cbadae37e09dee63f86d
Thomas Krille <tkrille@gmail.com>
2014-01-07 16:56:52 +0100

    [enhancement] added equals, hashCode and toString

--- scim-schema/src/main/java/org/osiam/resources/scim/MultiValuedAttributeType.java
+++ scim-schema/src/main/java/org/osiam/resources/scim/MultiValuedAttributeType.java
@@ -33,4 +33,43 @@ public abstract class MultiValuedAttributeType {
     public String getValue() {
         return value;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        MultiValuedAttributeType other = (MultiValuedAttributeType) obj;
+        if (value == null) {
+            if (other.value != null) {
+                return false;
+            }
+        } else if (!value.equals(other.value)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("MultiValuedAttributeType [value=").append(value).append(", getClass()=").append(getClass())
+                .append("]");
+        return builder.toString();
+    }
+
 }
