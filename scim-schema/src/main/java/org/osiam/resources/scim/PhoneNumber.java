2ad75a26300f50b814c864da1081bda0c772893e
Thomas Krille <tkrille@gmail.com>
2014-01-07 16:36:49 +0100

    [enhancement] added types for multi-valued attributes

---
+++ scim-schema/src/main/java/org/osiam/resources/scim/PhoneNumber.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2013 tarent AG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package org.osiam.resources.scim;
+
+public class PhoneNumber {
+
+    /**
+     * Represents a phone number type. Canonical values are available as static constants.
+     */
+    public static class Type extends MultiValuedAttributeType {
+        public static final Type WORK = new Type("work");
+        public static final Type HOME = new Type("home");
+        public static final Type MOBILE = new Type("mobile");
+        public static final Type FAX = new Type("fax");
+        public static final Type PAGER = new Type("pager");
+        public static final Type OTHER = new Type("other");
+
+        public Type(String value) {
+            super(value);
+        }
+    }
+
+}
