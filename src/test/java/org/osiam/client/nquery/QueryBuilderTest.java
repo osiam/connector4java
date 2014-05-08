41c80fd0e3ec955a0545ae80b0b8dfca4077e182
Thomas Krille <tkrille@gmail.com>
2014-05-08 23:00:04 +0200

    [task] added minimal new query api

--- src/test/java/org/osiam/client/nquery/QueryBuilderTest.java
+++ src/test/java/org/osiam/client/nquery/QueryBuilderTest.java
@@ -10,11 +10,11 @@ import org.junit.Test;
 public class QueryBuilderTest {
 
     QueryBuilder queryBuilder = new QueryBuilder();
-    
+
     @Test
     public void build_empty_query() {
         Query query = queryBuilder.build();
-        
+
         assertThat(query.getAttributes(), isEmptyOrNullString());
         assertThat(query.getFilter(), isEmptyOrNullString());
         assertThat(query.getSortBy(), isEmptyOrNullString());
@@ -22,7 +22,7 @@ public class QueryBuilderTest {
         assertThat(query.getStartIndex(), is(1L));
         assertThat(query.getCount(), is(100));
     }
-    
+
     @Test
     public void setting_all_values_of_the_query_works() {
         Query query = queryBuilder
@@ -30,16 +30,64 @@ public class QueryBuilderTest {
                 .filter("filter")
                 .sortBy("sortBy")
                 .sortOrder("sortOrder")
-                .startIndex(11)
+                .startIndex(11L)
                 .count(10)
                 .build();
-        
+
         assertThat(query.getAttributes(), is(equalTo("attributes")));
         assertThat(query.getFilter(), is(equalTo("filter")));
         assertThat(query.getSortBy(), is(equalTo("sortBy")));
         assertThat(query.getSortOrder(), is(equalTo("sortOrder")));
         assertThat(query.getStartIndex(), is(11L));
         assertThat(query.getCount(), is(10));
+    }
+
+    @Test
+    public void using_the_copy_of_constructor_works() {
+        Query queryOriginal = queryBuilder
+                .attributes("attributes")
+                .filter("filter")
+                .sortBy("sortBy")
+                .sortOrder("sortOrder")
+                .startIndex(11L)
+                .count(10)
+                .build();
+
+        Query queryCopy = new QueryBuilder(queryOriginal).build();
+
+        assertThat(queryCopy.getAttributes(), is(equalTo(queryOriginal.getAttributes())));
+        assertThat(queryCopy.getFilter(), is(equalTo(queryOriginal.getFilter())));
+        assertThat(queryCopy.getSortBy(), is(equalTo(queryOriginal.getSortBy())));
+        assertThat(queryCopy.getSortOrder(), is(equalTo(queryOriginal.getSortOrder())));
+        assertThat(queryCopy.getStartIndex(), is(queryOriginal.getStartIndex()));
+        assertThat(queryCopy.getCount(), is(queryOriginal.getCount()));
+    }
+
+    @Test
+    public void start_index_cannot_be_less_than_one() {
+        Query query = queryBuilder
+                .startIndex(0)
+                .build();
+
+        assertThat(query.getStartIndex(), is(1L));
+    }
+
+    @Test
+    public void count_cannot_be_less_than_one() {
+        Query query = queryBuilder
+                .count(0)
+                .build();
+
+        assertThat(query.getCount(), is(100));
+    }
+
+    @Test
+    public void count_cannot_be_greater_than_one_hundred() {
+        Query query = queryBuilder
+                .count(101)
+                .build();
+
+        assertThat(query.getCount(), is(100));
     }
 
 }
