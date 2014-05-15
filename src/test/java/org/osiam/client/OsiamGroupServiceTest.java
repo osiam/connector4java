906a5c00e0df3d9ed174af6e93b038c4a9c78b3e
Thomas Krille <tkrille@gmail.com>
2014-05-15 23:56:57 +0200

    [fix] removed wiremock
    wiremock and jersey 2.8 don't seem to get along with each other, so one
    must go.
    btw: much of the stuff we've tested using wiremock can be found in the
    integration tests anyway.
    
    [cleanup] reworked almost all tests

--- src/test/java/org/osiam/client/OsiamGroupServiceTest.java
+++ src/test/java/org/osiam/client/OsiamGroupServiceTest.java
@@ -23,302 +23,82 @@
 
 package org.osiam.client;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static org.apache.http.HttpStatus.SC_CONFLICT;
-import static org.apache.http.HttpStatus.SC_NOT_FOUND;
-import static org.apache.http.HttpStatus.SC_OK;
-import static org.apache.http.HttpStatus.SC_UNAUTHORIZED;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
-import java.util.Set;
-
-import javax.ws.rs.core.MediaType;
-
-import junit.framework.Assert;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.osiam.client.exception.NoResultException;
-import org.osiam.client.exception.UnauthorizedException;
 import org.osiam.client.oauth.AccessToken;
 import org.osiam.resources.scim.Group;
-import org.osiam.resources.scim.MemberRef;
-
-import com.github.tomakehurst.wiremock.client.MappingBuilder;
-import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 public class OsiamGroupServiceTest {
 
-    private static final String URL_BASE = "/osiam-server/Groups";
-    @Rule
-    public WireMockRule wireMockRule = new WireMockRule(9090); // No-args constructor defaults to port 8080
-
     private static final String GROUP_ID_STRING = "55bbe688-4b1e-4e4e-80e7-e5ba5c4d6db4";
-    private static final String INVALID_GROUP_ID_STRING = "55bbe688-4b1e-4e4e-80e7-e5ba5c4d";
-    private static final String USER_ID_STRING = "94bbe688-4b1e-4e4e-80e7-e5ba5c4d6db4";
     private static final String ENDPOINT = "http://localhost:9090/osiam-server";
-    private static final int NUMBER_OF_EXPECTED_GROUPS = 7;
 
     private String searchedId;
     private AccessToken accessToken;
-    private AccessTokenMockProvider tokenProvider;
-    private Group singleGroupResult;
-    private List<Group> allGroups;
     private OsiamGroupService service;
 
     @Before
     public void setUp() throws IOException {
         service = new OsiamGroupService.Builder(ENDPOINT).build();
-        tokenProvider = new AccessTokenMockProvider("/__files/valid_accesstoken.json");
-
-        givenAGroupID();
-        givenAnAccessToken();
-    }
-
-    @Test
-    public void existing_group_is_returned() throws IOException {
-        givenIDcanBeFound();
-        whenSingleGroupIsLookedUp();
-        thenReturnedGroupHasID(searchedId);
-    }
-
-    @Test
-    public void group_has_valid_values() throws Exception {
-        givenIDcanBeFound();
-        whenSingleGroupIsLookedUp();
-        thenReturnedGroupIsAsExpected();
-    }
-
-    @Test
-    public void list_of_groups_is_returned() throws Exception {
-        givenAllGroupsAreLookedUpSuccessfully();
-        whenAllGroupsAreLookedUp();
-        thenReturnedListOfAllGroupsIsAsExpected();
+        searchedId = GROUP_ID_STRING;
+        accessToken = new AccessToken.Builder("c5d116cb-2758-4e7c-9aca-4a115bc4f19e")
+                .setExpiresAt(new Date(System.currentTimeMillis() * 2))
+                .build();
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void id_is_null_by_getting_single_user_raises_exception() throws Exception {
-        givenIDisEmpty();
-        searchedId = null;
-        whenSingleGroupIsLookedUp();
+        service.getGroup(null, accessToken);
+
         fail("Exception expected");
     }
 
     @Test(expected = NullPointerException.class)
     public void accessToken_is_null_by_getting_single_group_raises_exception() throws Exception {
-        givenIDisEmpty();
-        accessToken = null;
-        whenSingleGroupIsLookedUp();
+        service.getGroup(searchedId, null);
+
         fail("Exception expected");
     }
 
     @Test(expected = NullPointerException.class)
     public void accessToken_is_null_by_getting_all_group_raises_exception() throws Exception {
-        givenIDisEmpty();
-        accessToken = null;
-        whenAllGroupsAreLookedUp();
+        service.getAllGroups(null);
+
         fail("Exception expected");
-    }
-
-    @Test(expected = NoResultException.class)
-    public void group_does_not_exist() throws IOException {
-        givenIDcanNotBeFound();
-        whenSingleGroupIsLookedUp();
-        fail("Exception expected");
-    }
-
-    @Test(expected = NoResultException.class)
-    public void invalid_ID_search() throws IOException {
-        givenIDisInvalid();
-        whenSingleGroupIsLookedUp();
-        fail("Exception expected");
-    }
-
-    @Test(expected = NoResultException.class)
-    public void invalid_ID_is_star() throws IOException {
-        givenIDisSpecial("*");
-        whenSingleGroupIsLookedUp();
-        fail("Exception expected");
-    }
-
-    @Test(expected = NoResultException.class)
-    public void invalid_ID_is_dot() throws IOException {
-        givenIDisSpecial(".");
-        whenSingleGroupIsLookedUp();
-        fail("Exception expected");
-    }
-
-    @Test(expected = UnauthorizedException.class)
-    public void expired_access_token() throws Exception {
-        givenExpiredAccessTokenIsUsedForLookup();
-        whenSingleGroupIsLookedUp();
-        fail("Exception expected");
-    }
-
-    @Test(expected = UnauthorizedException.class)
-    public void invalid_access_token() throws Exception {
-        givenInvalidAccessTokenIsUsedForLookup();
-        whenSingleGroupIsLookedUp();
-        fail("Exception expected");
-    }
-
-    @Test (expected = NullPointerException.class)
-    public void create_null_group_raises_exception(){
-        Group newGroup = null;
-        service.createGroup(newGroup, accessToken);
-        Assert.fail("Exception excpected");
-    }
-
-    @Test (expected = NullPointerException.class)
-    public void create_group_with_null_accestoken_raises_exception(){
-        Group newGroup = new Group.Builder().build();
-        service.createGroup(newGroup, null);
-        Assert.fail("Exception excpected");
-    }
-
-    @Test (expected = IllegalArgumentException.class)
-    public void delete_null_group_raises_exception(){
-        String groupID = null;
-        service.deleteGroup(groupID, accessToken);
-        Assert.fail("Exception excpected");
     }
 
     @Test(expected = NullPointerException.class)
-    public void delete_group_with_null_accestoken_raises_exception(){
-        String id = "HelloWorld";
-        service.deleteGroup(id, null);
-        Assert.fail("Exception excpected");
+    public void create_null_group_raises_exception() {
+        service.createGroup(null, accessToken);
+
+        fail("Exception excpected");
     }
 
-    private void givenAnAccessToken() throws IOException {
-        accessToken = tokenProvider.valid_access_token();
+    @Test(expected = NullPointerException.class)
+    public void create_group_with_null_accestoken_raises_exception() {
+        Group newGroup = new Group.Builder().build();
+
+        service.createGroup(newGroup, null);
+
+        fail("Exception excpected");
     }
 
-    private void givenAGroupID() {
-        searchedId = GROUP_ID_STRING;
+    @Test(expected = IllegalArgumentException.class)
+    public void delete_null_group_raises_exception() {
+        service.deleteGroup(null, accessToken);
+
+        fail("Exception excpected");
     }
 
-    private void whenSingleGroupIsLookedUp() {
-        singleGroupResult = service.getGroup(searchedId, accessToken);
-    }
+    @Test(expected = NullPointerException.class)
+    public void delete_group_with_null_accestoken_raises_exception() {
+        service.deleteGroup("irrelevant", null);
 
-    private void whenAllGroupsAreLookedUp() {
-        allGroups = service.getAllGroups(accessToken);
-    }
-
-    private void givenExpiredAccessTokenIsUsedForLookup() {
-        stubFor(givenIDisLookedUp(GROUP_ID_STRING, accessToken)
-                .willReturn(aResponse()
-                        .withStatus(SC_UNAUTHORIZED)
-                        .withHeader("WWW-Authenticate", "None")));
-    }
-
-    private void givenInvalidAccessTokenIsUsedForLookup() {
-        stubFor(givenIDisLookedUp(GROUP_ID_STRING, accessToken)
-                .willReturn(aResponse()
-                        .withStatus(SC_UNAUTHORIZED)
-                        .withHeader("WWW-Authenticate", "None")));
-    }
-
-    private void givenIDcanNotBeFound() {
-        stubFor(givenIDisLookedUp(GROUP_ID_STRING, accessToken)
-                .willReturn(aResponse()
-                        .withStatus(SC_NOT_FOUND)));
-    }
-
-    private void givenIDisInvalid() {
-        stubFor(givenIDisLookedUp(INVALID_GROUP_ID_STRING, accessToken)
-                .willReturn(aResponse()
-                        .withStatus(SC_NOT_FOUND)));
-    }
-
-    private void givenIDisSpecial(String wildcard) {
-        stubFor(givenIDisLookedUp(wildcard, accessToken)
-                .willReturn(aResponse()
-                        .withStatus(SC_CONFLICT)));
-    }
-
-    private void givenIDcanBeFound() {
-        stubFor(givenIDisLookedUp(GROUP_ID_STRING, accessToken)
-                .willReturn(aResponse()
-                        .withStatus(SC_OK)
-                        .withHeader("Content-Type", MediaType.APPLICATION_JSON)
-                        .withBodyFile("group_" + GROUP_ID_STRING + ".json")));
-    }
-
-    private void givenIDisEmpty() {
-        stubFor(givenIDisLookedUp("", accessToken)
-                .willReturn(aResponse()
-                        .withStatus(SC_OK)
-                        .withHeader("Content-Type", MediaType.APPLICATION_JSON)
-                        .withBodyFile("query_all_groups.json")));
-    }
-
-    private void givenAllGroupsAreLookedUpSuccessfully() {
-        stubFor(get(urlEqualTo(URL_BASE + "?count=2147483647"))
-                .withHeader("Accept", equalTo(MediaType.APPLICATION_JSON))
-                .withHeader("Authorization", equalTo("Bearer " + accessToken.getToken()))
-                .willReturn(aResponse()
-                        .withStatus(SC_OK)
-                        .withHeader("Content-Type", MediaType.APPLICATION_JSON)
-                        .withBodyFile("query_all_groups.json")));
-    }
-
-    private MappingBuilder givenIDisLookedUp(String id, AccessToken accessToken) {
-        return get(urlEqualTo(URL_BASE + "/" + id))
-                .withHeader("Accept", equalTo(MediaType.APPLICATION_JSON))
-                .withHeader("Authorization", equalTo("Bearer " + accessToken.getToken()));
-    }
-
-    private void thenReturnedGroupHasID(String id) {
-        Group result = service.getGroup(id, accessToken);
-        assertEquals(id.toString(), result.getId());
-    }
-
-    private void thenReturnedListOfAllGroupsIsAsExpected() {
-        assertEquals(NUMBER_OF_EXPECTED_GROUPS, allGroups.size());
-        for (Group currentGroup : allGroups) {
-            if (currentGroup.getId().equals(GROUP_ID_STRING)) {
-                assertEquals(1, currentGroup.getMembers().size());
-                for (MemberRef actValue : currentGroup.getMembers()) {
-                    assertEquals(USER_ID_STRING, actValue.getValue().toString());
-                }
-                break;
-            }
-        }
-    }
-
-    private void thenReturnedGroupIsAsExpected() throws Exception {
-
-        assertEquals(GROUP_ID_STRING, singleGroupResult.getId());
-        assertEquals("Group", singleGroupResult.getMeta().getResourceType());
-
-        Date created = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2011-08-01 20:29:49");
-
-        assertEquals(created, singleGroupResult.getMeta().getCreated());
-        assertEquals(created, singleGroupResult.getMeta().getLastModified());
-        assertEquals("test_group01", singleGroupResult.getDisplayName());
-
-        Set<MemberRef> users = singleGroupResult.getMembers();
-        int count = 0;
-        for (MemberRef multiValuedAttribute : users) {
-            Object value = multiValuedAttribute.getValue();
-            assertTrue(value.getClass().equals(String.class));
-            String userId = multiValuedAttribute.getValue();
-            assertEquals(USER_ID_STRING, userId);
-            count++;
-        }
-        assertEquals(1, count);
+        fail("Exception excpected");
     }
 }
--- src/test/java/org/osiam/client/oauth/AccessTokenTest.java
+++ src/test/java/org/osiam/client/oauth/AccessTokenTest.java
@@ -28,36 +28,34 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.Date;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
 import nl.jqno.equalsverifier.Warning;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.osiam.client.AccessTokenMockProvider;
 
 public class AccessTokenTest {
 
     private final static String TOKEN = "c5d116cb-2758-4e7c-9aca-4a115bc4f19e";
-    private AccessTokenMockProvider tokenProvider;
     private AccessToken accessToken;
-
-    @Before
-    public void setUp() {
-        tokenProvider = new AccessTokenMockProvider("/__files/valid_accesstoken.json");
-
-    }
 
     @Test
     public void access_token_is_deserialized_without_errors() throws IOException {
-        given_a_valid_access_token();
+        accessToken = new AccessToken.Builder("c5d116cb-2758-4e7c-9aca-4a115bc4f19e")
+                .setExpiresAt(new Date(System.currentTimeMillis() * 2))
+                .build();
+
         assertEquals(TOKEN, accessToken.getToken());
         assertFalse(accessToken.isExpired());
     }
 
     @Test
     public void expired_access_token_is_recognized_correctly() throws Exception {
-        given_an_expired_access_token();
+        accessToken = new AccessToken.Builder("c5d116cb-2758-4e7c-9aca-4a115bc4f19e")
+                .setExpiresAt(new Date(Long.MIN_VALUE))
+                .build();
+
         assertTrue(accessToken.isExpired());
     }
 
@@ -65,16 +63,8 @@ public class AccessTokenTest {
     public void equalsContract() {
         EqualsVerifier.forClass(AccessToken.class)
                 .usingGetClass()
-                .suppress(Warning.NULL_FIELDS, Warning.NONFINAL_FIELDS) // Neither Null nor modification is possible
+                .suppress(Warning.NULL_FIELDS, Warning.NONFINAL_FIELDS)
                 .verify();
-    }
-
-    private void given_an_expired_access_token() throws Exception {
-        accessToken = tokenProvider.expired_access_token();
-    }
-
-    private void given_a_valid_access_token() throws IOException {
-        accessToken = tokenProvider.valid_access_token();
     }
 
 }
