780aab3224f1af7700d2c800153198463da6d0a1
Florian Wallner <wallner@balumba.org>
2015-05-24 00:58:03 +0200

    Small clean-ups for code hygiene
    This pull request is a little clean up of the code base.
     - Replaces a deprecated method call with its intended successor
     - Removes an unused parameter from a protected method
     - Removes declared exceptions from signatures if broader parent exceptions are already defined
     - Uses the diamond inference since java 7 is required
     - replaces an unnecessary use of StringBuffer with StringBuilder for performance.

--- src/main/java/org/osiam/client/helper/ScopeDeserializer.java
+++ src/main/java/org/osiam/client/helper/ScopeDeserializer.java
@@ -29,21 +29,19 @@ import java.util.Set;
 import org.osiam.client.oauth.Scope;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
 public class ScopeDeserializer extends JsonDeserializer<Set<Scope>> {
 
     @Override
-    public Set<Scope> deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException,
-            JsonProcessingException {
+    public Set<Scope> deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
         String text = jp.getText();
         return parseParameterList(text);
     }
 
     private Set<Scope> parseParameterList(String values) {
-        Set<Scope> scopeResult = new HashSet<Scope>();
+        Set<Scope> scopeResult = new HashSet<>();
         if (values != null && values.trim().length() > 0) {
             String[] scopes = values.split("\\s+");
             for (String scope : scopes) {
--- src/main/java/org/osiam/client/helper/ScopeSerializer.java
+++ src/main/java/org/osiam/client/helper/ScopeSerializer.java
@@ -29,17 +29,15 @@ import java.util.Set;
 import org.osiam.client.oauth.Scope;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
 public class ScopeSerializer extends JsonSerializer<Set<Scope>> {
 
     @Override
-    public void serialize(Set<Scope> value, JsonGenerator jgen, SerializerProvider provider) throws IOException,
-            JsonProcessingException {
+    public void serialize(Set<Scope> value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
         if (value != null && !value.isEmpty()) {
-            StringBuffer scopeResult = new StringBuffer();
+            StringBuilder scopeResult = new StringBuilder();
             for (Scope scope : value) {
                 scopeResult.append(scope).append(" ");
             }
